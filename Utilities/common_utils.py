4352893b31ac593fd5731b297ac5d8575ec9a52d
sudhirnqa <sudhir.natarajan93@gmail.com>
2025-04-30 22:16:01 +0530

    Initial Framework Structure

---
+++ Utilities/common_utils.py
@@ -0,0 +1,71 @@
+import datetime
+import inspect
+import logging
+
+import allure
+import faker
+import random
+
+
+def allure_attach_screenshot(driver):
+    """Attach a screenshot to the Allure report."""
+    # Get the name of the test function
+    test_name = inspect.stack()[1][3]
+    # Attach the screenshot to the Allure report
+    now = datetime.datetime.now()
+    formated_now = (now.strftime('%Y-%m-%d-%H-%M-%S-%f') +
+                    str(int(now.strftime('%f')) * random.randrange(1, 100)))
+    test_name = f"{test_name}_{formated_now}"
+    allure.attach(
+        driver.get_screenshot_as_png(),
+        name=test_name,
+        attachment_type=allure.attachment_type.PNG
+    )
+
+
+def custom_logger(log_level=logging.DEBUG):
+    """Create and configure a custom logger."""
+    name = inspect.stack()[1][3]
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        logger.setLevel(log_level)
+        file_handler = logging.FileHandler("AutomationRun.log", mode='a')
+        file_format = logging.Formatter(
+            fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%m-%d-%Y %H:%M:%S"
+        )
+        file_handler.setFormatter(file_format)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+def get_fake_demographics():
+    """Generate fake demographic data."""
+    log = custom_logger()
+    try:
+        fake = faker.Faker()
+        demographics = {
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "email": fake.email(),
+            "phone_number": fake.phone_number(),
+            "address_1": fake.street_address(),
+            "address_2": fake.secondary_address(),
+            "city": fake.city(),
+            "state": fake.state(),
+            "zip_code": fake.zipcode(),
+            "password": fake.password(),
+            "date_of_birth": fake.date_of_birth(minimum_age=18, maximum_age=100),
+            "company_name": fake.company(),
+        }
+
+        # Log the generated demographics
+        log.info(f"Generated demographics: {demographics}")
+
+    except Exception as e:
+        log.error(f"Error generating fake demographics: {e}")
+        raise
+
+    return demographics
